1c4228bea1aa9669621eec50ec12c876e4f85ed7
seung Lee <jfmam@naver.com>
2021-11-02 20:22:54 +0900

    add responsive

--- Layout/mainlayout.tsx
+++ Layout/mainlayout.tsx
@@ -5,21 +5,36 @@ import router from 'next/router';
 import { Header } from '../components/Header';
 
 import { IconButton } from '@components/button';
+import { useLayout } from 'hooks/layout';
 
 interface MainLayoutProps {
   children: ReactNode;
 }
 
 export default function MainLayout({ children }: MainLayoutProps) {
+  const { width } = useLayout();
+
   return (
     <>
       <Header>
         <div style={{ display: 'flex', justifyContent: 'space-between', flexBasis: '130rem' }}>
           <IconButton onClick={() => router.push('/')}>
-            <Image src="/logo.svg" alt={undefined} width="152" height="23rem" loading="eager" />
+            <Image
+              src="/logo.svg"
+              alt={undefined}
+              width={width > 768 ? 152 : 112}
+              height={width > 768 ? 23 : 17}
+              loading="eager"
+            />
           </IconButton>
           <IconButton>
-            <Image src="/menu.svg" alt="menu" width="28rem" height="23rem" loading="eager" />
+            <Image
+              src="/menu.svg"
+              alt="menu"
+              width={width > 768 ? 28 : 18}
+              height={width > 768 ? 26 : 16}
+              loading="eager"
+            />
           </IconButton>
         </div>
       </Header>
---
+++ hooks/layout/index.ts
@@ -0,0 +1,22 @@
+import { useState, useEffect, useCallback } from 'react';
+import debounce from 'lodash.debounce';
+
+export function useLayout() {
+  const [width, setWidth] = useState<number>(0);
+
+  const onResizeWidth = useCallback((w: number) => {
+    setWidth(w);
+  }, []);
+
+  useEffect(() => {
+    const debounceResize = () => debounce(() => onResizeWidth(window.innerWidth), 500);
+
+    window.addEventListener('resize', debounceResize());
+
+    return () => {
+      window.removeEventListener('resize', debounceResize());
+    };
+  }, [onResizeWidth]);
+
+  return { width };
+}
