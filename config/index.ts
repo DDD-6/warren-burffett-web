94e0033018322d7e95de235dd188ceaeff1513c0
dolphin <ksmself@gmail.com>
2021-11-05 19:35:44 +0900

    add change API_URL / test signup

--- config/index.ts
+++ config/index.ts
@@ -1,4 +1,4 @@
 export const configs = {
-  API_URL: process.env.API_URL || 'http://localhost:8080',
+  API_URL: process.env.API_URL || 'http://3.37.71.132:8080',
   ENV: process.env.NODE_ENV || 'development',
 };
---
+++ pages/signup.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from 'next';
+
+import SignUp from '@components/SignUp';
+
+const SignUpPage: NextPage = () => {
+  return <SignUp />;
+};
+
+export default SignUpPage;
