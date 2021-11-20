7c21857e9f790aad8c9d04d4c57b82c370cfb8e0
seung Lee <jfmam@naver.com>
2021-11-20 19:58:40 +0900

    fix config file typo

--- next.config.js
+++ next.config.js
@@ -11,8 +11,8 @@ module.exports = {
     figmaUrl: process.env.NEXT_PUBLIC_FIGMA_URL,
     SERVER_URL: process.env.NEXT_PUBLIC_SERVER_URL,
     SERVER_PROTOCOL: process.env.NEXT_PUBLIC_SERVER_PROTOCOL,
-    API_PROTOCOL = process.env.NEXT_PUBLIC_API_PROTOCOL,
-    API_URL =  process.env.NEXT_PUBLIC_API_URL,
+    API_PROTOCOL: process.env.NEXT_PUBLIC_API_PROTOCOL,
+    API_URL: process.env.NEXT_PUBLIC_API_URL,
   },
   pageExtensions: ['page.tsx', 'page.jsx', 'tsx'],
   eslint: {
