5b2740a8c06773aa6e32e547c0ac0e9a6b89f3aa
seung Lee <jfmam@naver.com>
2021-11-22 22:05:34 +0900

    add env domain protocol

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports = {
   images: {
     formats: ['image/avif', 'image/webp'],
     ...process.env.NODE_ENV === "production" && {
-      path:`${process.env.NEXT_PUBLIC_API_PROTOCOL}://${process.env.NEXT_PUBLIC_DOMAIN}`,
+      path:`${process.env.NEXT_PUBLIC_DOMAIN_PROTOCOL}://${process.env.NEXT_PUBLIC_DOMAIN}`,
       loader: 'imgix',
     }
   },
