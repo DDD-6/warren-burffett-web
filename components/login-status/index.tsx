b104b68bcdae1709a0a3d694b875aeab7e6ee4fa
seung Lee <jfmam@naver.com>
2021-11-27 12:51:25 +0900

    login test

--- components/login-status/index.tsx
+++ components/login-status/index.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react';
 import router from 'next/router';
 import Image from 'next/image';
 import { useQuery } from 'react-query';
@@ -8,18 +7,11 @@ import { IconButton } from 'components';
 import { Token } from 'common/type/user';
 
 export default function LoginStatus() {
-  const [isLogin, setIsLogin] = useState(false);
   const { data } = useQuery('login-status', () => getLocalStorageItem<Token>('token'));
-
-  useEffect(() => {
-    if (data?.accessToken) {
-      setIsLogin(true);
-    }
-  }, [data?.accessToken]);
 
   return (
     <>
-      {isLogin ? (
+      {data?.accessToken ? (
         <Image src="/user_login.svg" alt="menu" width={153} height={40} loading="eager" />
       ) : (
         <IconButton style={{ padding: 0 }} onClick={() => router.push('/signin')}>
